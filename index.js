c6b96ef8a0d74ff750b5d33952616334d2d97962
Luis Escobar <lae311@stern.nyu.edu>
2021-10-09 16:37:44 -0400

    commit tree 50de329dd1a361a7761ecf53925538fb0264b2a9

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  let result
+  if (number < 400) {
+    result = "This one is on me!";
+  }
+  else if (number > 2000 && number < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (number > 2500) {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result
+  if (city == "NYC") {
+    result = "Ok, sounds good.";
+  }
+  else {
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+      break;
+  }
+  return result
 }
